6154069264af2887ff849fdb15ed215c5df7f9eb
Michał Walczak <mjf.walczak@gmail.com>
2013-03-08 20:03:23 +0100

    slightly modified the build script

--- bnrunStabby.sh
+++ bnrunStabby.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 make clean > /dev/null
+if [ $? -eq 0 ]
+    then
+        echo 'Cleanup finished, building...'
+    else
+        echo 'Cleanup fail.'
+        exit
+fi
 
 make > /dev/null
 
